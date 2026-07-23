eb3b1411883266968910b583da43dd51050e5d12
lamartinecabral <lamartine.cb@gmail.com>
2026-07-22 22:45:59 -0300

    Remove unnecessary indexes from sobrenomes table in schema.sql

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -31,35 +31,6 @@ create table if not exists public.sobrenomes (
   constraint sobrenomes_pkey primary key (nome)
 ) TABLESPACE pg_default;
 
-create index if not exists idx_sobrenomes_freq_br on public.sobrenomes (freq_br);
-create index if not exists idx_sobrenomes_freq_ac on public.sobrenomes (freq_ac);
-create index if not exists idx_sobrenomes_freq_al on public.sobrenomes (freq_al);
-create index if not exists idx_sobrenomes_freq_am on public.sobrenomes (freq_am);
-create index if not exists idx_sobrenomes_freq_ap on public.sobrenomes (freq_ap);
-create index if not exists idx_sobrenomes_freq_ba on public.sobrenomes (freq_ba);
-create index if not exists idx_sobrenomes_freq_ce on public.sobrenomes (freq_ce);
-create index if not exists idx_sobrenomes_freq_df on public.sobrenomes (freq_df);
-create index if not exists idx_sobrenomes_freq_es on public.sobrenomes (freq_es);
-create index if not exists idx_sobrenomes_freq_go on public.sobrenomes (freq_go);
-create index if not exists idx_sobrenomes_freq_ma on public.sobrenomes (freq_ma);
-create index if not exists idx_sobrenomes_freq_mg on public.sobrenomes (freq_mg);
-create index if not exists idx_sobrenomes_freq_ms on public.sobrenomes (freq_ms);
-create index if not exists idx_sobrenomes_freq_mt on public.sobrenomes (freq_mt);
-create index if not exists idx_sobrenomes_freq_pa on public.sobrenomes (freq_pa);
-create index if not exists idx_sobrenomes_freq_pb on public.sobrenomes (freq_pb);
-create index if not exists idx_sobrenomes_freq_pe on public.sobrenomes (freq_pe);
-create index if not exists idx_sobrenomes_freq_pi on public.sobrenomes (freq_pi);
-create index if not exists idx_sobrenomes_freq_pr on public.sobrenomes (freq_pr);
-create index if not exists idx_sobrenomes_freq_rj on public.sobrenomes (freq_rj);
-create index if not exists idx_sobrenomes_freq_ro on public.sobrenomes (freq_ro);
-create index if not exists idx_sobrenomes_freq_rn on public.sobrenomes (freq_rn);
-create index if not exists idx_sobrenomes_freq_rr on public.sobrenomes (freq_rr);
-create index if not exists idx_sobrenomes_freq_rs on public.sobrenomes (freq_rs);
-create index if not exists idx_sobrenomes_freq_sc on public.sobrenomes (freq_sc);
-create index if not exists idx_sobrenomes_freq_se on public.sobrenomes (freq_se);
-create index if not exists idx_sobrenomes_freq_sp on public.sobrenomes (freq_sp);
-create index if not exists idx_sobrenomes_freq_to on public.sobrenomes (freq_to);
-
 create table if not exists public.localidades (
   cod integer not null,
   nome character varying not null,
